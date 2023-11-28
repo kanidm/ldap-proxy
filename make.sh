0f5598a9936f4593e4e123e70087752468ebef84
James Hodgkinson <james@terminaloutcomes.com>
2023-11-29 09:34:27 +1000

    more checks

--- make.sh
+++ make.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 cargo vendor 1> ./cargo_config
+if [ ! -f "SUSE_CA_Root.pem" ]; then
+    echo "Couldn't find the SUSE Root CA, touching it"
+    touch SUSE_CA_Root.pem
+fi
 docker buildx build --pull --push --platform "linux/amd64" \
     -f ./Dockerfile \
     -t firstyear/ldap-proxy:latest \
