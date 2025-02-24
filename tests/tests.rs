438f4cb2cbdc67dbfd5327b94e7bd519b8f4d5df
James Hodgkinson <james@terminaloutcomes.com>
2025-02-24 13:47:50 +1000

    fix: changing to JSON config

--- tests/tests.rs
+++ tests/tests.rs
@@ -14,10 +14,14 @@ fn hello_world() {
 fn test_config_load() {
     assert!(toml::from_str::<Config>("").is_err());
 
-    assert!(toml::from_str::<Config>(include_str!("test_config.toml")).is_ok());
-    let config = toml::from_str::<Config>(include_str!("test_config.toml")).unwrap();
+    let load_config = serde_json::from_str::<Config>(include_str!("test_config.json"));
+    dbg!(&load_config);
+    assert!(load_config.is_ok());
 
-    assert_eq!(config.ldap_ca.to_str(), Some("/etc/ldap-proxy/ldap-ca.pem"));
+    assert_eq!(
+        load_config.expect("Failed to load config").ldap_ca.to_str(),
+        Some("/etc/ldap-proxy/ldap-ca.pem")
+    );
 }
 
 #[test]
