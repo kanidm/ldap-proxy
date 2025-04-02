7159da98554c540f5f1e53c4eb844244cabda33c
William Brown <william@blackhats.net.au>
2025-04-02 17:42:49 +1000

    Proxy header demo complete

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
-use std::collections::BTreeMap;
-use std::net::SocketAddr;
-use std::path::PathBuf;
-use std::time::Duration;
-
 use concread::arcache::ARCache;
 use hashbrown::HashSet;
+use ldap3_proto::parse_ldap_filter_str;
 use ldap3_proto::{LdapFilter, LdapSearchScope};
 use openssl::ssl::SslConnector;
 use serde::Deserialize;
+use serde_with::DeserializeFromStr;
+use std::collections::BTreeMap;
+use std::net::SocketAddr;
+use std::path::PathBuf;
+use std::str::FromStr;
+use std::time::Duration;
 use url::Url;
 
 pub mod proxy;
@@ -26,12 +28,28 @@ pub struct AppState {
     pub max_incoming_ber_size: Option<usize>,
     pub max_proxy_ber_size: Option<usize>,
     pub allow_all_bind_dns: bool,
+    pub remote_ip_addr_info: AddrInfoSource,
 }
 
 #[derive(Debug, Clone, Deserialize, Default)]
 pub struct DnConfig {
     #[serde(default)]
-    pub allowed_queries: HashSet<(String, LdapSearchScope, LdapFilter)>,
+    pub allowed_queries: HashSet<(String, LdapSearchScope, LdapFilterWrapper)>,
+}
+
+#[derive(DeserializeFromStr, Debug, Clone, PartialEq, Eq, Hash)]
+pub struct LdapFilterWrapper {
+    pub inner: LdapFilter,
+}
+
+impl FromStr for LdapFilterWrapper {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        parse_ldap_filter_str(s)
+            .map(|inner| LdapFilterWrapper { inner })
+            .map_err(|err| err.to_string())
+    }
 }
 
 fn default_cache_bytes() -> usize {
@@ -40,6 +58,13 @@ fn default_cache_bytes() -> usize {
 
 fn default_cache_entry_timeout() -> u64 {
     1800
+}
+
+#[derive(Debug, Deserialize, Default, Clone, Copy)]
+pub enum AddrInfoSource {
+    #[default]
+    None,
+    ProxyV2,
 }
 
 #[derive(Debug, Deserialize)]
@@ -55,6 +80,9 @@ pub struct Config {
 
     pub ldap_ca: PathBuf,
     pub ldap_url: Url,
+
+    #[serde(default)]
+    pub remote_ip_addr_info: AddrInfoSource,
 
     pub max_incoming_ber_size: Option<usize>,
     pub max_proxy_ber_size: Option<usize>,
